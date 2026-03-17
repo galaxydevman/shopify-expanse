7351759ef4e5f17849cb5862effd1c4d463fe06c
@ntony <tjfriersondream@gmail.com>
2026-03-17 10:25:48 -0400

    change shop by category more smaller circle

--- assets/section.featured-collections.js
+++ assets/section.featured-collections.js
@@ -23,6 +23,27 @@ class FeaturedCollections extends HTMLThemeElement {
     }
 
     this.flickity = new Slideshow(this.slideshow, defaults)
+
+    if (this.flickity && this.flickity.slideshow) {
+      const prevButton = this.slideshow.querySelector('.flickity-prev-next-button.previous')
+      const nextButton = this.slideshow.querySelector('.flickity-prev-next-button.next')
+      const totalSlides = this.flickity.slideshow.cells ? this.flickity.slideshow.cells.length : 0
+
+      const updateArrows = () => {
+        const index = this.flickity.slideshow.selectedIndex || 0
+
+        if (prevButton) {
+          prevButton.disabled = index === 0
+        }
+
+        if (nextButton) {
+          nextButton.disabled = totalSlides > 0 ? index >= totalSlides - 1 : false
+        }
+      }
+
+      updateArrows()
+      this.flickity.slideshow.on('change', updateArrows)
+    }
   }
 
   disconnectedCallback() {
